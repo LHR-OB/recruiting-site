d41406ee1387b821c64b3d7c2c6369d718b85f40
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2023-05-09 02:52:34 -0500

    updates dashboard page slightly

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -17,7 +17,16 @@ export default function Dashboard({ user, setSnackbarData }) {
         }}
       >
         <Typography variant="h4" mt={2}>
-          Welcome, {user ? user.first_name: ""}
+          Welcome to the Longhorn Racing Application Portal!
+        </Typography>
+        <Typography variant="body1" mt={2}>
+          Make sure to log in or create an account to get started.
+        </Typography>
+        <Typography variant="body1" mt={2}>
+          If you are an applicant, you can use the navigation bar on the side to submit an application, schedule interviews, and view the status of your application.
+        </Typography>
+        <Typography variant="body1" mt={2}>
+          If you are a current member, you can use the navigation bar on the side to view and mark applications, schedule interviews, and manage your team.
         </Typography>
       </Box>
     </Container>
