01224a388f800ab54c05252c14316febea67e1b5
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2023-05-09 15:22:35 -0500

    attempt to revert since the issue was the browser

--- server/main.py
+++ server/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
-from starlette.middleware import Middleware
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-import os
 
 from routes import users, applications, events, teams, availabilities, interviews, messages
 from database.database import Base, engine
@@ -11,24 +9,18 @@ from utils.users import authenticate_user, create_access_token
 from utils.dummy_data import main as create_dummy_data
 from dependencies import get_db, required_interviewer
 
-# Middleware
-middleware = [
-    Middleware(
-        CORSMiddleware,
-        allow_origins=[
-            'http://localhost:3000',
-            'https://lhr-recruitingclient-test.up.railway.app/',
-            os.environ.get('CLIENT_URL')
-        ],
-        allow_credentials=True,
-        allow_methods=['*'],
-        allow_headers=['*']
-    )
-]
-
 # Main app configuration
 Base.metadata.create_all(bind=engine)
-app = FastAPI(middleware=middleware)
+app = FastAPI()
+
+# Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 # Routers
 app.include_router(users.router)
