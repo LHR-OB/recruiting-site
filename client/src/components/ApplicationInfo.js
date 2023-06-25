884d69cb7ee53bdb7633df52bb50518654220637
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2023-06-24 19:28:04 -0500

    adds application summary per team

--- client/src/components/ApplicationInfo.js
+++ client/src/components/ApplicationInfo.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import {
   Button,
   Container,
@@ -7,14 +7,30 @@ import {
 
 import CenterModal from './CenterModal';
 import EditApplicationInfoForm from './EditApplicationInfoForm';
+import { applicationsApi, applicationCyclesApi } from '../api/endpoints/applications';
 
 export default function ApplicationInfo({ team, setTeam, setTeams, setSnackbarData }) {
   // States
   const [open, setOpen] = useState(false);
+  const [applications, setApplications] = useState([]);
 
   const handleEditApplicationInfo = () => {
     setOpen(true);
   }
+
+  useEffect(() => {
+    if (team) {
+      applicationCyclesApi.getApplicationCycleActive().then((res) => {
+        if (res.status === 200) {
+          applicationsApi.getApplicationsByTeam(res.data.id, team.id).then((res) => {
+            if (res.status === 200) {
+              setApplications(res.data);
+            }
+          })
+        }
+      })
+    }
+  }, [team]);
 
   return (
     <Container>
@@ -22,7 +38,16 @@ export default function ApplicationInfo({ team, setTeam, setTeams, setSnackbarDa
         Application Info
       </Typography>
       <Typography variant="body1" mt={2}>
-        TODO: Application Summary Goes Here
+        Applications: {applications.length}
+      </Typography>
+      <Typography variant="body1" mt={2}>
+        Stage Accepted: {applications.filter((application) => application.stage_decision === 'ACCEPTED').length}
+      </Typography>
+      <Typography variant="body1" mt={2}>
+        Stage Rejected: {applications.filter((application) => application.stage_decision === 'REJECTED').length}
+      </Typography>
+      <Typography variant="body1" mt={2}>
+        Stage Neutral: {applications.filter((application) => application.stage_decision === 'NEUTRAL').length}
       </Typography>
       <Button
         variant="outlined"
