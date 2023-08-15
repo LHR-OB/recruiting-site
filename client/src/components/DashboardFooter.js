06f8cdecfe26b490138c472660feff9614f83547
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2023-08-14 21:41:35 -0400

    my own little touch

--- client/src/components/DashboardFooter.js
+++ client/src/components/DashboardFooter.js
@@ -1,6 +1,7 @@
 import { React } from 'react';
 import {
     Box,
+    Grid,
     List,
     ListItemButton,
     ListItemIcon,
@@ -42,31 +43,63 @@ export default function DashboardFooter() {
                 backgroundColor: '#F5F5F5',
             }}
         >
-            <Box
-                sx={{
-                    paddingTop: 2,
-                    marginLeft: 8,
-                }}
-            >
-                <Typography variant="h6">
-                    Contact Information
-                </Typography>
-                    <List>
-                    {FOOTER_LINKS.map((item) => (
-                        <ListItemButton
-                            key={item.label}
-                            onClick={() => window.open(item.link, '_blank')}
+            <Grid container>
+                <Grid item xs={2}>
+                    <Box
+                        sx={{
+                            paddingTop: 2,
+                            marginLeft: 8,
+                        }}
+                    >
+                        <Typography variant="h6">
+                            Contact Information
+                        </Typography>
+                            <List>
+                            {FOOTER_LINKS.map((item) => (
+                                <ListItemButton
+                                    key={item.label}
+                                    onClick={() => window.open(item.link, '_blank')}
+                                >
+                                    <ListItemIcon>
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <Typography variant="body1">
+                                        {item.label}
+                                    </Typography>
+                                </ListItemButton>
+                            ))}
+                        </List>
+                    </Box>
+                </Grid>
+                <Grid item xs={8} />
+                <Grid item xs={2}>
+                    <Box
+                        sx={{
+                            paddingTop: 2,
+                            marginRight: 8,
+                        }}
+                    >
+                        <Typography variant="body2">
+                            © 2023 Longhorn Racing
+                        </Typography>
+                        <Typography variant="body2">
+                            This site was developed with love by Rishi Ponnekanti (LHR Solar Captain 2022-2023)
+                        </Typography>
+                        <Typography variant="body2">
+                            If anything breaks, it's not his fault
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            sx={{
+                                marginTop: 2,
+                                fontStyle: 'italic',
+                            }}
                         >
-                            <ListItemIcon>
-                                {item.icon}
-                            </ListItemIcon>
-                            <Typography variant="body1">
-                                {item.label}
-                            </Typography>
-                        </ListItemButton>
-                    ))}
-                </List>
-            </Box>
+                            "Autobots, roll out!"
+                        </Typography>
+                    </Box>
+                </Grid>
+            </Grid>
         </Box>
     );
 }
