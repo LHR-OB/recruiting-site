b8f52355f59a6ebd0049e5464353721dd3eb538f
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2023-08-18 12:54:38 -0400

    adds approved check to protected page

--- client/src/components/ProtectedPage.js
+++ client/src/components/ProtectedPage.js
@@ -4,7 +4,7 @@ import consts from '../config/consts';
 
 export default function ProtectedPage({ user, requiredRole, children }) {
     const capsRoles = consts.USER_ROLES.map((role) => role.toUpperCase().replace(' ', '_'));
-    if (user && capsRoles.indexOf(user.type) >= 0 && capsRoles.indexOf(user.type) <= capsRoles.indexOf(requiredRole)) {
+    if (user && user.status === "APPROVED" && capsRoles.indexOf(user.type) >= 0 && capsRoles.indexOf(user.type) <= capsRoles.indexOf(requiredRole)) {
         return children;
     } else {
         return <Unauthorized />;
