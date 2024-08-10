9309972ece88039bdee5f58f5a9ecc2bca37f39d
Anna <annafsagui@gmail.com>
2024-08-09 23:39:35 -0500

    condition on applications.js for app

--- client/src/pages/Applications.js
+++ client/src/pages/Applications.js
@@ -81,7 +81,7 @@ export default function Applications({ user, setSnackbarData }) {
           setApplications={setApplications}
           setSnackbarData={setSnackbarData}
         />
-        {user?.type === 'APPLICANT' && applicationCycle?.stage === 'APPLICATION' && !applicationCycleClosed &&
+        {user?.type === 'APPLICANT' && !applicationCycleClosed &&
         <>
           <Typography variant="body1" mt={2}>
             You may submit up to one application for each system on each team. If you would like to apply for multiple systems within the same team at the same time, please select all the desired systems in your application. Once you submit an application, you will not be able to make any changes, so make sure to double check your application before submitting.
